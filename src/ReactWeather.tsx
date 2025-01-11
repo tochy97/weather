e3fadd7f39222e9037ce523d06594808c77b2a08
tochy97 <tochyegeonu97@gmail.com>
2025-01-10 19:14:56 -0600

    Using css transform to rotate arrow. added auto-prefixer; Published 2.3.2

--- src/ReactWeather.tsx
+++ src/ReactWeather.tsx
@@ -105,11 +105,11 @@ export default class ReactWeather extends Component<Props, State> {
                 this.index > 0 &&
                 <svg
                   className="button"
-                  width="20px"
-                  height="20px"
+                  width="1em"
+                  height="1em"
                   stroke="currentColor"
                   viewBox="0 0 24 24"
-                  fill="none"
+                  fill="currentColor"
                   xmlns="http://www.w3.org/2000/svg"
                   onClick={this.prevDayForcast}
                 >
@@ -120,14 +120,13 @@ export default class ReactWeather extends Component<Props, State> {
               {
                 this.index < 15 &&
                 <svg
-                  className="button"
-                  width="20px"
-                  height="20px"
+                  className="button right"
+                  width="1em"
+                  height="1em"
                   stroke="currentColor"
                   viewBox="0 0 24 24"
-                  fill="none"
+                  fill="currentColor"
                   xmlns="http://www.w3.org/2000/svg"
-                  transform="matrix(-1, 0, 0, 1, 0, 0)"
                   onClick={this.nextDayForcast}
                 >
                   <path xmlns="http://www.w3.org/2000/svg" d="M16.1795 3.26875C15.7889 2.87823 15.1558 2.87823 14.7652 3.26875L8.12078 9.91322C6.94952 11.0845 6.94916 12.9833 8.11996 14.155L14.6903 20.7304C15.0808 21.121 15.714 21.121 16.1045 20.7304C16.495 20.3399 16.495 19.7067 16.1045 19.3162L9.53246 12.7442C9.14194 12.3536 9.14194 11.7205 9.53246 11.33L16.1795 4.68297C16.57 4.29244 16.57 3.65928 16.1795 3.26875Z" fill="#0F0F0F" />
